77345b045fd30a331683f59f45685c742395197a
Tedd OKANO <okano.akifumi@gmail.com>
2023-05-27 05:24:33 +0900

    docs updated

--- docs/search/functions_4.js
+++ docs/search/functions_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['main_0',['main',['../namespacecharacter__pattern.html#adea2aab93a60d9764198ec700d6bebce',1,'character_pattern']]]
+  ['main_0',['main',['../namespacecharacter__pattern.html#a0086c498ba34fd4dba4196471507a097',1,'character_pattern']]]
 ];
